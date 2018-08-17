9e03926bfdb0289e7eea7a7ae101d16c982b5146
Hassan Mccutchen <mccutchen.hassan@gmail.com>
2018-08-17 14:59:01 -0400

    created sign up form

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 require 'sinatra/activerecord'
-
+ enable :sessions
 
 set :database, 'sqlite3:loginlesson.sqlite3'
 
@@ -14,9 +14,36 @@ erb :home
   end
 
 
+get '/account' do
+
+
+erb :account
+end
+
+
+get '/signup' do
+
+erb :signup
+end
 
 
 
+post '/signup' do
+
+p params
+end
+
+
+post '/login' do
+
+@email = params[:email]
+@password = params[:password]
+
+p @email
+p @password
+
+redirect :account
+end
 
 
 get '/login' do
